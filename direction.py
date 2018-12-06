b1bc3c6c716a39ea06e90272aef934816cd2b4ca
chanson20 <chanson20@bsmschool.org>
2018-12-06 11:18:15 -0600

    added functions for analog sensing speed

--- direction.py
+++ direction.py
@@ -4,15 +4,32 @@ import setup
 L = pin
 R = pin
 
+#converts a value 1-10 with a speed
 def convert(speed):
   return(22.13*speed+1389.35)
+
+#Default Direction
 def front(input):
   speed = convert(input)
   RPL.servoWrite(L,speed)
   RPL.servoWrite(R,speed)
 def left(input):
-  speed = convert(input)
-  RPL.servoWrite(R,speed)
+  RPL.servoWrite(R,convert(input))
 def right(input):
-  speed = convert(input)
-  RPL.servoWrite(L,speed)
+  RPL.servoWrite(L,convert(input))
+
+
+#Analog direction
+def analogfront(input):
+  RPL.servoWrite(L,leftanalog(input))
+  RPL.servoWrite(R,rightanalog(input))
+def analogright(input):
+  RPL.servoWrite(R,rightanalog(input))
+def analogleft(input):
+  RPL.servoWrite(L,leftanalog(input))
+
+#Analog reading to speed converter
+def leftanalog(input):
+  return(-0.23*input+1617)
+def rightanalog(input):
+  return(0.23*reading+1383)
