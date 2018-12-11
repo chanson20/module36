8fd39cb24be9d0578b356b351bc5eff7483ee7b8
chanson20 <chanson20@bsmschool.org>
2018-12-11 11:45:44 -0600

    Fully functioning touch.py with comments

--- touch.py
+++ touch.py
@@ -1,13 +1,13 @@
 import direction
 import time
 import RoboPiLib as RPL
-pause = TIME HERE
-speed = SPEED HERE
-apin = 7
+pause = 0.53 #Time after autopilot goes away
+speed = 1460 #Speed after autopilot goes away
+apin = 7 #Analog Sensor 84.18mm from the front
 
 while True:
   analog = RPL.analogRead(apin)
-  if analog <= 650: #EDIT THIS
+  if analog <= 600: #Autopilot
     direction.touch(analog)
   else:
     direction.frontraw(speed)
