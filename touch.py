add6a7bd765030a4ac237c6974a04e878f58d187
chanson20 <chanson20@bsmschool.org>
2018-12-11 09:37:26 -0600

    touching up touch.py

--- touch.py
+++ touch.py
@@ -1,14 +1,16 @@
 import direction
 import time
+import RoboPiLib as RPL
 pause = TIME HERE
+speed = SPEED HERE
 apin = 7
 
 while True:
   analog = RPL.analogRead(apin)
-  if analog <= 650:
+  if analog <= 650: #EDIT THIS
     direction.touch(analog)
   else:
-    direction.frontraw(1505) #EDIT THIS
+    direction.frontraw(speed)
     time.sleep(pause)
     direction.stop()
     break
